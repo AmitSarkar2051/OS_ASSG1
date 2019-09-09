fd5df6da8914eba7fd3bad4ec1bfb1d1446f65d7
amitsarkar2051 <amitsarkar2051@gmail.com>
2019-09-09 17:16:19 +0530

    monday

--- 2019201040_Assg1.c
+++ 2019201040_Assg1.c
@@ -10,13 +10,203 @@
 #include<sys/types.h>
 #include<pwd.h>
 //#include "al_map.h"
+
+//global var
+char s[1024];
+char* argv[100];
+char path[50];
+char cmd[1024];
+
 char _lalias[100][10];
 char _ralias[100][50];
 int _calias=0;
 
+int io=0;
+int pip[2];
 
-//using namespace std;
-//
+
+//functions
+void pip_handle(char*);
+void set_alarm(int);
+void alarm_handle(int);
+int al_srch(char*);
+void makealias(char*);
+void al_arr_update();
+char* al_str_cleansing();
+void fover_write(char*,char*);
+void fappend_write(char*,char*);
+void fappend_write_cmd(char*);
+void show_history();
+void clear_history();
+void add_history(char*);
+void bashrccreation();
+void ext_parse(char *, char*, int);
+void pip_exec(int , char**);
+
+void ext_parse(char *s, char* tar, int ch)
+{
+		printf("\nEXT PARSE start with : ---%s--------------\n",s);
+		char *p = strchr(s, '\n');
+                if (p) *p = 0;
+		printf("\nEXT PARSE start with : ---%s--------------\n",s);
+		int i=0	;
+		int flg=0;
+		struct passwd *pwd = getpwuid(getuid());  // Check for NULL!
+		char* usr= pwd->pw_name;
+		char hst[100+1];
+		gethostname(hst, sizeof(hst));  // Check the return value!
+		char sym='$';	
+		//char bin[]="/bin/";
+		char bin[]="";
+		strcpy(cmd,bin);
+		char line[1024];
+		char * args1[100];
+		strcpy(line,s);
+		printf("\nline in exPAr:----%s---\n",line);
+		char* s1=strtok(line," ");
+		printf("\n  In expar s1 val:-----%s-----\n",s1);
+	
+		int  _falias=al_srch(s1);
+                if(_falias > -1){
+			printf("\n . .. .. . %d.. . .. .\n",_falias);
+			 char alline[100];
+                         strcpy(alline,_ralias[_falias]);
+			 char* als1=strtok(alline," ");
+			  printf("\nOut while. ..  %d . .. %s\n",_falias,als1);
+			 //args1[0]=als1;
+			 s1=als1;
+			 strcat(cmd,als1);
+			 while(als1 != NULL){
+			 	//als1 = strtok (NULL, " "i);
+				printf("\nIn while. ..  %d . .. %s\n",i,als1);
+				args1[i++]=als1;
+			 	als1 = strtok (NULL, " ");
+			 }
+		
+		}
+		else if(_falias <= -1){
+			strcat(cmd,s1);
+		 	args1[i++]=s1;
+		}
+		_falias=-1;
+		     printf("\n s1 val: %s",s1);
+	 printf("\nf val: %d\n",_falias);
+		while (s1!= NULL)
+  		{
+			if(!strcmp(s1,"$HOME")){
+				printf("%s\n",pwd->pw_dir);
+				flg=1;
+				break;
+			}
+			   if(!strcmp(s1,"alias")){
+                                char* al=strtok(NULL,"\n");
+                                makealias(al);
+                                flg=1;
+                                break;
+                        }
+    			s1 = strtok (NULL, " ");
+			printf("\n********************** s1 val: %s\n",s1);
+			if(s1!=NULL)
+				_falias=al_srch(s1);
+			if(_falias > -1){
+			
+                	        printf("\n . .. .. . %d.. . .. .\n",_falias);
+                        	char alline[100];
+                         	strcpy(alline,_ralias[_falias]);
+                         	char* als1=strtok(alline," ");
+                          	printf("\nOut while. ..  %d . .. %s\n",_falias,als1);
+                         	s1=als1;
+                         	while(als1 != NULL){
+                                //als1 = strtok (NULL, " "i);
+                                	printf("\nIn while. ..  %d . .. %s\n",i,als1);
+                                	args1[i++]=als1;
+                                	als1 = strtok (NULL, " ");
+                         	}
+
+                
+
+			}
+			printf("\n**************************** s1 val: %s\n",s1);
+			if(_falias <= -1){
+                        	args1[i++]=s1;
+	                }
+                	_falias=-1;
+		args1[i]=(char*)0;
+
+		if(flg==1)
+			continue;
+        	int pid= fork();
+		if(pid<0){
+			printf("\nERROR: in pid\n");
+			return ;
+		}
+	              //fork child
+        	if(pid==0) {
+			switch(ch)
+			{
+				case 1:
+					// > overwrite implementation
+					{
+					printf("\nIN CASE 1\n");
+					int f2 =open(tar,O_RDONLY | O_WRONLY | O_TRUNC|O_CREAT,0777);
+        				if(f2<0){
+                				printf("Error in target file open .  .. \n");
+                				return;
+        				}
+					dup2(f2, STDOUT_FILENO);
+				        close(f2);
+					break;
+					}
+				case 2:
+					{
+
+					printf("\nIN CASE 2\n");
+					// > append implementation
+					int f2 =open(tar,O_WRONLY|O_APPEND|O_CREAT,0777);
+        				if(f2<0){
+                				printf("Error in target file open .  .. \n");
+                				return;
+        				}
+					dup2(f2, STDOUT_FILENO);
+				        close(f2);
+					break;
+					}
+				case 3:
+					{
+					printf("\nIN CASE 3\n");
+					//  implementation
+					if(io!=0){
+						dup2(io,0);
+				        	close(io);
+					}
+					if(pip[1]!=1){
+						dup2(pip[1],1);
+				        	close(pip[1]);
+					}
+					break;
+					}
+				case 4:
+					{
+					printf("\nIN CASE 4\n");
+					break;
+					}
+				default:
+					break;
+
+
+			}		
+            		int ex = execvp(cmd,args1);
+            		if(ex<0){
+				perror("exec");
+  	          		exit(1);
+			}  
+		      } else {                    //Parent
+            			wait(NULL);
+        			}
+    }
+    return ;
+};
+
 void bashrccreation(){
 	char tar[]="mybashrc";
 	FILE* fbrc =fopen(tar,"w");
@@ -26,15 +216,17 @@ void bashrccreation(){
         }
 	struct passwd *pwd = getpwuid(getuid());
        	char hst[100+1];
-        gethostname(hst, sizeof(hst));	
+        gethostname(hst, sizeof(hst));
+	char cwd[100+1];
+	getcwd(cwd, sizeof(cwd));
 	char sym='$';
 	
 	fprintf (fbrc,"user : %s\n", pwd->pw_name);
 	fprintf (fbrc,"host : %s\n", hst);
 	fprintf (fbrc,"HOME : %s\n", pwd->pw_dir);
 	fprintf (fbrc,"PS1 : %c\n", sym);
-	fprintf (fbrc,"PWD : %s\n", pwd->pw_name);
-	fprintf (fbrc,"PATH : %s\n", pwd->pw_name);
+	fprintf (fbrc,"PWD : %s\n", cwd);
+	//fprintf (fbrc,"PATH : %s\n", pwd->pw_name);
 	fclose(fbrc);
                 /*struct passwd *pwd = getpwuid(getuid());  // Check for NULL!
                 char* usr= pwd->pw_name;
@@ -94,13 +286,47 @@ void show_history(){
               wait(NULL);
         }
 }
+void fappend_write_cmd(char* tar){
 
+	/*int f2 =open(tar,O_WRONLY|O_APPEND|O_CREAT,0777);
+        //int f2 =open(tar,O_RDONLY | O_WRONLY | O_TRUNC);
+        if(f2<0){
+                printf("Error in target file open .  .. \n");
+                return;
+        }
+	*/	//string seperation
+	char *f=s, *b=s;
+	strsep(&b,">");
+	printf("\nsep ..  . .. %s\n",f);
+	ext_parse(f,tar,1);
+	//free(f);
+	//free(b);
+    	return ;
+}
+void fover_write_cmd(char* tar){
+
+	/*int f2 =open(tar,O_RDONLY | O_WRONLY | O_TRUNC|O_CREAT,0777);
+        //int f2 =open(tar,O_RDONLY | O_WRONLY | O_TRUNC);
+        if(f2<0){
+                printf("Error in target file open .  .. \n");
+                return;
+        }*/
+	char *f=s, *b=s;
+	strsep(&b,">");
+	printf("\nsep ..  . .. %s\n",f);
+	ext_parse(f,tar,2);
+	//free(f);
+	//free(b);
+	return;
+
+}
 void fappend_write(char* src,char* tar){
-	//printf("\nSource:%s",src);
-	//printf("\nTarget:%s",tar);
+	printf("\nSource:%s",src);
+	printf("\nTarget:%s",tar);
 	 int f1 =open(src,O_RDONLY);
         if(f1<0){
                 printf("Error in reading .  .. \n");
+		fappend_write_cmd(tar);
                 return;
         }
         int f2 =open(tar,O_WRONLY|O_APPEND|O_CREAT,0777);
@@ -127,6 +353,7 @@ void fover_write(char* src,char* tar){
 	int f1 =open(src,O_RDONLY);
 	if(f1<0){
 		printf("Error in reading .  .. \n");
+		fover_write_cmd(tar);		
 		return;
 	}
 	//int f2 =open(tar,O_WRONLY|O_CREAT,0777);
@@ -158,7 +385,7 @@ char* al_str_cleansing()
 	int d=0;
 	while (rht[c] != '\0')
    	{
-      	if (!(rht[c] == '=' || rht[c] == '"')) {
+      	if (!(rht[c] == '=' || rht[c] == '"'|| rht[c] == '\'')) {
         	cln[d] = rht[c];
         d++;
       }
@@ -232,7 +459,43 @@ void set_alarm(int alarm_tm){
     	printf("\nALARM: alarm stopped at %d. . . . .\n",alarm_tm);
     	return ;
 };
+void pip_exec(int x, char** pargs){
+	int i=0;
+	printf("============% d===========",x);
+	for (i=0;i<(x-1);i++){
+		pipe(pip);
+	printf("===========*****=% d===========",i);
+		ext_parse(pargs[i],NULL,3);
+		close(pip[1]);
+		io=pip[0];	
+	}
+	if((io!=0)&&(i==x-1))
+		dup2(io,0);
+	ext_parse(pargs[i],NULL,4);	
+};
+void pip_handle(char* s){
+	printf("\n . .. .. . PIPE CALLED.. . .. .\n");
+        char pline[1000];
+	char* pargs[100];
+        strcpy(pline,s);
+	int i=0;
+	char *f=s, *b=s;
+	while(f!=NULL){
+		strsep(&b,"|");
+		printf("\nIn while. ..  . .. %s\n",f);
+                pargs[i++]=f;
+		f=b;
+	}
+	//free(f);
+	//free(b);
+	for(int j=0;j<i;j++)
+		printf("\npipe arg %d: %s \n",j,pargs[j]);
+	pip_exec(i,pargs);
 
+	printf("\nPipe done . . .\n");
+ 	strcpy(s,"\n");
+	return ;
+};
  
 int main(){
 	//for fgets
@@ -246,9 +509,9 @@ int main(){
         	perror("Unable to allocate buffer");
         	exit(1);
     	}*/
-	char* argv[100];
-	char path[50];
-	char cmd[1024];
+//	char* argv[100];
+//	char path[50];
+//	char cmd[1024];
 //	char end[]="exit\n";
 //	char stp[]="STOP";
 
@@ -258,25 +521,36 @@ int main(){
 
 		/**/
 		int i=0;
-		struct passwd *pwd = getpwuid(getuid());  // Check for NULL!
+		struct passwd *pwd = getpwuid(getuid()); //user name
 		char* usr= pwd->pw_name;
 
 		char hst[100+1];
-		gethostname(hst, sizeof(hst));  // Check the return value!
+		gethostname(hst, sizeof(hst));//hostname/machine name
+		
+		char cwd[100+1];
+		getcwd(cwd, sizeof(cwd));//pwd
 
 		char sym='$';
 
-
-		printf("\n%s@%s:%c",usr,hst,sym);
+		
+		printf("\n%s@%s:~%s%c",usr,hst,cwd,sym);
 		/**/
 		int flg=0;	
-		fflush(stdin); 
-		fflush(stdout); 
+		//fflush(stdin); 
+		//fflush(stdout); 
 		//if(!getline(&s,&s_size,stdin))
-		 char s[1024];
+		// char s[1024];
 		if(!fgets(s,1024,stdin))
 			break;
-		add_history(s);
+		add_history(s); //history maintain
+		char pip[]=" | ";
+		char *pipfind = strstr(s, pip);
+                if (pipfind) {
+			pip_handle(s);
+			flg=1;
+		}
+		if(strcmp(s,"\n")==0)
+                                continue;
 		if(strcmp(s,"exit\n")==0)
                                 break;
 		if(!strcmp(s,"history\n")){
@@ -303,14 +577,26 @@ int main(){
 		execvp(prompt[0],prompt);
 		 ****************************/
 		char nouse[]="";
-		char bin[]="/bin/";
+		//char bin[]="/bin/";
+		char bin[]="";
 		strcpy(cmd,bin);
 		char line[1024];
 		char * args1[100];
 		strcpy(line,s);
 		char* s1=strtok(line," ");
 		printf("\n *******************s1 val: %s\n",s1);
-		
+		//cd implementation
+		if(!strcmp(s1,"cd")){
+			char* s2=strtok(NULL," ");
+			int e= chdir(s2);
+			if(e!=0){
+				printf("ERROR: Unable to cd . Error %d",e);
+			}
+			printf("\nChanged the directory to %s",s2);
+			flg=1;
+                        continue;
+
+		 }
 		//ALARM
 		if(!strcmp(s1,"alarm")){
 				repeat=10;
@@ -398,15 +684,15 @@ int main(){
 
 			   //     overwrite >
 			if(!strcmp(s1,">")){
-				char* src=args1[i-1];
-				char* tar=strtok(NULL," ");
+				char* src=args1[i-2]; //token i -1
+				char* tar=strtok(NULL," ");// token i+1
 				fover_write(src,tar);
 				flg=1;
 				break;
 			}
 			// append >>
 			if(!strcmp(s1,">>")){
-                                char* src=args1[i-1];
+                                char* src=args1[i-2];
                                 char* tar=strtok(NULL," ");
                                 fappend_write(src,tar);
 				flg=1;
@@ -471,9 +757,11 @@ int main(){
         	if(pid==0) {
 	 		//printf("\n Path:%s args1:%s-%s-%s-%s \n",cmd,args1[0],args1[1],args1[2],args1[3]);
 			//printf("\n Path:%s args1 size:%d-%d-%d-%d \n",cmd,strlen(args1[0]),strlen(args1[1]),strlen(args1[2]),strlen(args1[3]));
-            		execvp(cmd,args1);
-            		perror("exec");
-            		exit(1);
+            		int ex = execvp(cmd,args1);
+			if(ex<0){
+            			perror("exec");
+            			exit(1);
+			}
         	} else {                    //Parent
             	wait(NULL);
         	}
